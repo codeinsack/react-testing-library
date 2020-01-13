fe3fe5eaab19779607b34915310f8c1cdef32b8e
Eugene Furmanov <yauhen.furmanau@codex-soft.com>
2020-01-13 16:18:40 +0300

    add mock api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import { FavoriteNumber } from "./components/FavoriteNumber"
+import { GreetingLoader } from "./components/GreetingLoader"
 
 function App() {
   return (
     <div className="App">
       <FavoriteNumber />
+      <GreetingLoader />
     </div>
   );
 }
