3904c63e8359bd836146ad7a580d52d3cacdbd1f
Eugene Furmanov <yauhen.furmanau@codex-soft.com>
2020-01-13 15:24:57 +0300

    add tests for form

--- src/components/FavoriteNumber.test.js
+++ src/components/FavoriteNumber.test.js
@@ -11,11 +11,9 @@ test('renders a number input with a label "Favorite Number"', () => {
 })
 
 test('entering an invalid value shows an error message', () => {
-  const { getByLabelText, getByRole, rerender, debug } = render(<FavoriteNumber />)
+  const { getByLabelText, getByRole, rerender } = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   fireEvent.change(input, { target: { value: 10 } })
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
-  debug();
   rerender(<FavoriteNumber max={10} />)
-  debug();
 })
